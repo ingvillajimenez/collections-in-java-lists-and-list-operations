a19c072c022404a81ecb4cd43a461d6639b9edfc
Juan Villa <ing.villa.jimenez@gmail.com>
2023-07-22 19:41:49 -0600

    Performing Collection Operations

--- JavaCollections/src/com/skillsoft/collections/Main.java
+++ JavaCollections/src/com/skillsoft/collections/Main.java
@@ -8,50 +8,88 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<Car> carList = new ArrayList<>();
+//        ArrayList<String> cityList = new ArrayList<>();
+        List<String> cityList = new ArrayList<>();
+//        Collection<String> cityList = new ArrayList<>(); // size, add, remove, contains, containsAll,
+                                                        // removeAll, addAll are part of the Collection interface
 
-        carList.add(new Car("Honda", "Civic", 21000.0));
-        carList.add(new Car("Toyota", "Camry", 29000.0));
-        carList.add(new Car("BMW", "5 Series", 65000.0));
+        System.out.println("************Adding elements");
 
-        System.out.println("Car list: " + carList);
-        System.out.println();
+        cityList.add("Bengaluru");
+        cityList.add("New York");
+        cityList.add("Mumbai");
 
-        ArrayList nonGenericCarList = new ArrayList<>();
-
-        nonGenericCarList.add(new Car("Honda", "Civic", 21000.0));
-        nonGenericCarList.add(new Car("Toyota", "Camry", 29000.0));
-        nonGenericCarList.add(new Car("BMW", "5 Series", 65000.0));
-
-        System.out.println("Non-Generic Car list: " + nonGenericCarList);
-        System.out.println();
-
-        System.out.println("**** No casting required to access list elements");
-
-        for (Car car: carList) {
-            System.out.format("%s, %s, %.1f", car.getMake(), car.getModel(), car.getPrice());
-            System.out.println();
-        }
+        System.out.println("cityList contents: " + cityList);
+        System.out.println("cityList size: " + cityList.size());
+        System.out.println("cityList isEmpty(): " + cityList.isEmpty());
 
         System.out.println();
 
-        System.out.println("**** List elements have to be cast to the right type");
+        System.out.println("************Removing elements");
 
-        for (Object object: nonGenericCarList) {
-            Car car = (Car) object;
+        cityList.remove("Seattle");
+        cityList.remove("Palo Alto");
 
-            System.out.format("%s, %s, %.1f", car.getMake(), car.getModel(), car.getPrice());
-            System.out.println();
-        }
-
-        System.out.println();
-
-        Car car = carList.get(2);
-        System.out.println(car);
+        System.out.println("cityList contents: " + cityList);
+        System.out.println("cityList size: " + cityList.size());
+        System.out.println("cityList isEmpty(): " + cityList.isEmpty());
 
         System.out.println();
 
-        car = (Car) nonGenericCarList.get(1);
-        System.out.println(car);
+        System.out.println("**************Checking for elements");
+
+        System.out.println("cityList contains New York: " + cityList.contains("New York"));
+        System.out.println("cityList contains Paris: " + cityList.contains("Paris"));
+        System.out.println("cityList contains Mumbai: " + cityList.contains("Mumbai"));
+
+        System.out.println("cityList size: " + cityList.size());
+
+        System.out.println();
+
+        System.out.println("*************Adding multiple elements");
+
+        List<String> anotherCityList = new ArrayList<>();
+        anotherCityList.add("Paris");
+        anotherCityList.add("London");
+
+        System.out.println("cityList original contents: " + cityList);
+        System.out.println("Adding elements: " + anotherCityList);
+
+        cityList.addAll(anotherCityList);
+
+        System.out.println("cityList contents: " + cityList);
+        System.out.println("cityList size: " + cityList.size());
+
+        System.out.println();
+
+        System.out.println("*************Checking for multiple elements");
+
+        System.out.println("cityList contains Paris, London: " + cityList.containsAll(anotherCityList));
+
+        anotherCityList.add("Tokyo");
+        System.out.println("cityList contains Paris, London, Tokyo: " + cityList.containsAll(anotherCityList));
+
+        System.out.println("************Removing multiple elements");
+
+        System.out.println("cityList original contents: " + cityList);
+        System.out.println("Removing elements: " + anotherCityList);
+
+        cityList.removeAll(anotherCityList);
+
+        System.out.println("cityList contents: " + cityList);
+
+//        ArrayList<int> intList = new ArrayList<int>(); // Type argument cannot be of primitive type
+//        ArrayList<Integer> intList = new ArrayList<Integer>();
+//
+//        intList.add(100);
+//        intList.add(200);
+//        intList.add(300);
+//        intList.add(400);
+//
+//        System.out.println("Integer list: " + intList);
+//
+//        intList.add(null);
+//
+//        System.out.println("Integer list (after adding null): " + intList);
     }
 }
